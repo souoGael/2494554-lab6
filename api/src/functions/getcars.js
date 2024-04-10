c8643868384d51e3469ff5154c722cd1636ab8c2
souoGael <2494554@students.wits.ac.za>
2024-04-10 21:44:43 +0100

    added

---
+++ api/src/functions/getcars.js
@@ -0,0 +1,11 @@
+const { app } = require('@azure/functions');
+const cars = require('./cars.json');
+
+app.http('getcars', {
+    methods: ['GET'],
+    route:'/cars',
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        return { body: JSON.stringify(cars) };
+    }
+});
